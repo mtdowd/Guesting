ee13fe85457138095134a9e951657bd1a39f8b12
MTD <mtdowd@gmail.com>
2014-12-04 10:24:48 -0500

    Adds manager role to user model with validation
    * Creates radio buttons for manager/bartender selection in new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
     if @user.valid?
       sign_in(@user)
-      redirect_to root_path
+      redirect_to new_profile_path
     else
       render :new
     end
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :manager)
   end
 end
